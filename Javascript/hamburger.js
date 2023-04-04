f877ea61721344c1d37ff46a63867ae57530d1ff
davidmolloy10 <24234273@edgehill.ac.uk>
2023-04-04 14:08:13 +0100

    Added 3rd project to the site and went through a design overhaul to move to a light theme

--- Javascript/hamburger.js
+++ Javascript/hamburger.js
@@ -1,9 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector(".body")
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+let body = document.querySelector(".body")
+let model = document.querySelector(".model");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     body.classList.toggle("stop-scrolling");
-}) 
+    model.classList.toggle("model-hidden");
+})
